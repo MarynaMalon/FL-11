26209c48532ac7173015e4d6ce60013d0ed7f644
Maryna Malon <maryna.malion@gmail.com>
2019-07-23 00:27:15 +0300

    end of 22.07.2019

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,17 +1,22 @@
 let rootNode = document.getElementById('root');
 let addButton = document.getElementById('addButton');
 let inputValue = document.getElementById('newAdd').value;
+let ul = document.getElementById('myList');
+const liMax = 10;
+
+
 
 addButton.onclick = 
 function newAction() {
     let li = document.createElement('li');
     li.className = 'listclass';
     let inputValue = document.getElementById('newAdd').value;
+    
+    if(inputValue!==''){
+
     li.innerHTML = '<input type="checkbox" class="checkIcon" onchange="checkme()">' + inputValue +
     '<i onclick="editAction()" class="material-icons" id="editIcon">create</i>' + 
     '<i onclick="deleteAction()" class="material-icons" id="deleteIcon">delete</i>';
-    const liMax = 10;
-    let ul = document.getElementById('myList');
     let listLength = ul.getElementsByClassName('listclass').length;
     if (listLength === liMax) {
         document.getElementById('notification').innerHTML = 'Maximum item per list are created!';
@@ -21,6 +26,7 @@ function newAction() {
         document.getElementById('myList').appendChild(li);
     }
     document.getElementById('newAdd').value = '';
+}
 }
 
 function checkme() {
